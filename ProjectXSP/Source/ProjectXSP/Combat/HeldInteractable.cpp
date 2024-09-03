55bd715160e4aef9b0f6702b1de625a011e65c09
Gabriel Lucena <gabriellucena23@gmail.com>
2024-09-02 21:56:18 -0300

    More tests with pixelation, began work on weapons

---
+++ ProjectXSP/Source/ProjectXSP/Combat/HeldInteractable.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HeldInteractable.h"
+
+// Sets default values
+AHeldInteractable::AHeldInteractable()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+}
+
+// Called when the game starts or when spawned
+void AHeldInteractable::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AHeldInteractable::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	
+}
+
+void AHeldInteractable::Interact()
+{
+
+	OnInteract();
+}
+
+
+void AHeldInteractable::SetHolder(AActor* holder, FName socket)
+{
+	if(IsValid(holder))
+	{
+		AttachToActor(holder, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, false), socket);
+		Holder = holder;
+		HolderSocket = socket;
+	}
+	
+	Holder = holder;
+}
+
---
+++ ProjectXSP/Source/ProjectXSP/Combat/ModularHitscanWeapon.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "HeldInteractable.h"
+#include "ModularHitscanWeapon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTXSP_API AModularHitscanWeapon : public AHeldInteractable
+{
+	GENERATED_BODY()
+	
+};
